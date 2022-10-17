645b9bff146d8e5f59b4cd43fa78160920473ef5
fahmikdp <fahmikurnia04@gmail.com>
2022-10-17 09:09:23 +0700

    update migration

---
+++ database/migrations/2022_10_12_040930_penjualan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('penjualan', function (Blueprint $table) {
+            $table->engine = 'innodb';
+            $table->integer('id_penjualan')->primary();
+            $table->date('tanggal')->default(now());
+            $table->time('jam_jual')->default(now());
+            $table->text('keterangan')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/migrations/2022_10_16_090552_user.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        {
+            Schema::create('user', function (Blueprint $table) {
+                $table->engine = 'innodb';
+                $table->char('id_user', 5)->primary();
+                $table->string('nama', 60);
+                $table->string('username')->unique();
+                $table->text('password');
+                $table->text('foto');
+                $table->char('level', 3);
+
+                $table->timestamps();
+
+                // foreign
+                $table
+                    ->foreign('level')
+                    ->references('id_level')
+                    ->on('level_user')
+                    ->cascadeOnDelete();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
